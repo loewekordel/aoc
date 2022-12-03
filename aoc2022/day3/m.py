dfb0b5c917c8f9b2addce388665f46e4c2911caf
loewekordel <geo@text.com>
2022-12-03 12:11:04 +0100

    day3

--- aoc2022/day3/m.py
+++ aoc2022/day3/m.py
@@ -1,12 +1,20 @@
-D = [l.strip() for l in open('input.txt')]
+from string import ascii_lowercase, ascii_uppercase
 
+p = {c: i for i, c in enumerate(ascii_lowercase + ascii_uppercase, 1)}
+
+D = [l.strip() for l in open("input.txt")]
+s1 = 0
 for d in D:
-    pass
+    assert len(d) % 2 == 0
+    i = len(d) // 2
+    c1, c2 = d[:i], d[i:]
+    c, = set(c1) & set(c2)
+    s1 += p[c]
 
-with open("input.txt") as f:
-    l = []
-    for line in f:
-        line = line.strip()
-        l.append(line)
+s2 = 0
+for i in range(0, len(D), 3):
+    c, = set(D[i]) & set(D[i + 1]) & set(D[i + 2])
+    s2 += p[c]
 
-print(l)
+print(s1)
+print(s2)
