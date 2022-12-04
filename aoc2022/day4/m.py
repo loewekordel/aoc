01bd5cfb0f5db72c325152b614adf50045ff9d5f
loewekordel <geo@text.com>
2022-12-04 09:59:12 +0100

    day4

--- aoc2022/day4/m.py
+++ aoc2022/day4/m.py
@@ -1,12 +1,15 @@
+import re
 D = [l.strip() for l in open('input.txt')]
 
+a1 = 0
+a2 = 0
 for d in D:
-    pass
-
-with open("input.txt") as f:
-    l = []
-    for line in f:
-        line = line.strip()
-        l.append(line)
-
-print(l)
+    s1, e1, s2, e2 = re.match(r"(\d+)-(\d+),(\d+)-(\d+)", d).groups()
+    s1 = {n for n in range(int(s1), int(e1) + 1)}
+    s2 = {n for n in range(int(s2), int(e2) + 1)}
+    if s1 <= s2 or s2 <= s1:
+        a1+=1
+    if s1 & s2:
+        a2+=1
+print(a1)
+print(a2)
