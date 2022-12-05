1c6d8e4d9bd29072ef8f683a9e3d2f24a0f569a3
loewekordel <>
2022-12-05 12:26:27 +0100

    day5

--- aoc2022/day5/m.py
+++ aoc2022/day5/m.py
@@ -1,12 +1,30 @@
-D = [l.strip() for l in open('input.txt')]
+import re
+from collections import defaultdict, deque
+from copy import deepcopy
 
-for d in D:
-    pass
+D = open('input.txt').read()
+stacks = defaultdict(deque)
+data, instr = D.split("\n\n")
 
-with open("input.txt") as f:
-    l = []
-    for line in f:
-        line = line.strip()
-        l.append(line)
+for d in data.splitlines()[:-1]:
+    for i, e in enumerate(d):
+        if e == "[":
+            stacks[i//4 + 1].appendleft(d[i+1])
 
-print(l)
+stacks = dict(sorted(stacks.items()))
+cmds = re.findall(r"move (\d+) from (\d+) to (\d+)", instr)
+
+stacks1 = deepcopy(stacks)
+for n, f, t in cmds:
+    for i in range(int(n)):
+        stacks1[int(t)].append(stacks1[int(f)].pop())
+
+stacks2 = deepcopy(stacks)
+for n, f, t in cmds:
+    tmp = deque()
+    for i in range(int(n)):
+        tmp.appendleft(stacks2[int(f)].pop())
+    stacks2[int(t)].extend(tmp)
+
+print("".join(v.pop() for v in stacks1.values()))
+print("".join(v.pop() for v in stacks2.values()))
