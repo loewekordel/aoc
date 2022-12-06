dd1ec489595a48c869dae80e281a4da167ff7714
loewekordel <>
2022-12-06 08:55:10 +0100

    day6

--- aoc2022/day6/m.py
+++ aoc2022/day6/m.py
@@ -1,12 +1,13 @@
-D = [l.strip() for l in open('input.txt')]
+D = open('input.txt').read()
 
-for d in D:
-    pass
-
-with open("input.txt") as f:
-    l = []
-    for line in f:
-        line = line.strip()
-        l.append(line)
-
-print(l)
+p1 = False
+p2 = False
+for i in range(len(D)):
+    if len(set(D[i:i + 4])) == 4 and not p1:
+        print(i + 4)
+        p1 = True
+    if len(set(D[i:i + 14])) == 14 and not p2:
+        print(i + 14)
+        p2 = True
+    if p1 and p2:
+        break
