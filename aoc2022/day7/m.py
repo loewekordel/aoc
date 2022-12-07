c42a9f1b6259205ddcf250cd4b50449cc4851545
loewekordel <>
2022-12-07 13:44:59 +0100

    day7

--- aoc2022/day7/m.py
+++ aoc2022/day7/m.py
@@ -1,12 +1,29 @@
+from collections import defaultdict
+DIR_MAX_SIZE = 100000
+TOTAL_SIZE = 70000000
+NEEDED_SIZE = 30000000
+
 D = [l.strip() for l in open('input.txt')]
 
+pwd = []
+sizes = defaultdict(int)
 for d in D:
-    pass
+    # print(d)
+    l = d.split()
+    if l[0] == "dir" or l[1] == "ls":
+        continue
+    elif l[1] == "cd":
+        if l[2] == "..":
+            pwd.pop()
+        else:
+            pwd.append(l[2])
+    else:
+        for i in range(1, len(pwd) + 1):
+            sizes["/".join(pwd[:i])] += int(l[0])
 
-with open("input.txt") as f:
-    l = []
-    for line in f:
-        line = line.strip()
-        l.append(line)
+    # print(pwd)
+    print(sizes)
 
-print(l)
+print(sum(v for v in sizes.values() if v <= DIR_MAX_SIZE))
+print(min(v for v in sizes.values() if TOTAL_SIZE -
+          sizes["/"] + v >= NEEDED_SIZE))
